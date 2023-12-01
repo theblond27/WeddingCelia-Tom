66e52fa7ea127f2bcd576b8efc6c43fea24327d6
Quentin De Smet <dsmet.q@gmail.com>
2023-12-01 18:34:56 -0500

    Change select,option for button on Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import i18n from '../i18n';
 
-export default function Modal({ onClose}) {
+export default function Modal({ onClose }) {
 
   const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
 
@@ -9,6 +9,7 @@ export default function Modal({ onClose}) {
         e.preventDefault();
         i18n.changeLanguage(e.target.value);   // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
         setSelectedLanguage(e.target.value);
+        onClose();
     }
 
 
@@ -16,12 +17,16 @@ export default function Modal({ onClose}) {
     // backdrop
     <div  className={'fixed inset-0 bg-cadre bg-opacity-30 backdrop-blur-sm flex justify-center items-center'}>
       {/* modal */}
-      <div className={'bg-cadre rounded-xl shadow p-12'} defaultValue={selectedLanguage}>
-        <select size='3'onChange={chooseLanguage} className="bg-cadre w-28 h-32 text-center text-s appearance-none">  
+      <div className={'bg-cadre rounded-xl shadow p-12 grid'} defaultValue={selectedLanguage} >
+        <button className="cursor-pointer mb-4 text-text text-xl" onClick={chooseLanguage} value="fr">Français</button>
+        <button className="cursor-pointer mb-4 text-text text-xl" onClick={chooseLanguage} value="en">English</button>
+        <button className="cursor-pointer text-text text-xl" onClick={chooseLanguage} value="jp">日本語</button>
+
+        {/* <select size='3'onChange={chooseLanguage} className="bg-cadre w-28 h-32 text-center text-s appearance-none">  
             <option className="cursor-pointer mb-4 text-text text-xl" onClick={onClose} value="fr">Français</option>
             <option className="cursor-pointer mb-4 text-text text-xl" onClick={onClose} value="en">English</option>
             <option className="cursor-pointer text-text text-xl" onClick={onClose} value="jp">日本語</option>
-        </select>
+        </select> */}
       </div>
     </div>
   )
